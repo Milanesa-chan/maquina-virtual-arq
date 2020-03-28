6598a05cc7e78e3ef09e52c257d8df0f1b1f25ab
Milanesa-chan <lucagutierrez@hotmail.com>
2020-03-27 21:35:33 -0300

    agregue un monton de cosas

--- mvc.c
+++ mvc.c
@@ -17,17 +17,64 @@ int esRotulo(char*);
 
 int contieneArg(int, char* [], char*);
 
+void crearListaMnemonicos(char[][5]);
+
 int main(int carg, char *args[]){
     printf("%d\n", carg);
 
     if(carg > 1){
-        FILE* arch = fopen(args[1], "rt");
-        int mostrar = !contieneArg(carg, args, "-o");
-        listaRotulos rotulos = NULL;
+        int mostrar;
+        listaRotulos rotulos;
+        FILE* arch;
+        int memoria[2016];
+        char listaMnemonicos[144][5];
+        crearListaMnemonicos(listaMnemonicos);
+        printf("%s\n", listaMnemonicos[1]);
+        printf("%s\n", listaMnemonicos[0]);
+        printf("%s\n", listaMnemonicos[143]);
+        exit(0);
+        arch = fopen(args[1], "rt");
+        mostrar = !contieneArg(carg, args, "-o");
+        rotulos = NULL;
         buscaRotulos(arch, &rotulos, mostrar);
+        rewind(arch); //Vuelve el archivo al principio
 
     }
     return 0;
+}
+
+void crearListaMnemonicos(char lista[][5]){
+    for(int i=0; i<144; i++){
+        strcpy(lista[i], "");
+    }
+
+    strcpy(lista[1], "MOV");
+    strcpy(lista[2], "ADD");
+    strcpy(lista[3], "SUB");
+    strcpy(lista[4], "MUL");
+    strcpy(lista[5], "DIV");
+    strcpy(lista[6], "MOD");
+    strcpy(lista[19], "CMP");
+    strcpy(lista[23], "SWAP");
+    strcpy(lista[25], "RND");
+    strcpy(lista[49], "AND");
+    strcpy(lista[50], "OR");
+    strcpy(lista[51], "NOT");
+    strcpy(lista[52], "XOR");
+    strcpy(lista[55], "SHL");
+    strcpy(lista[56], "SHR");
+    strcpy(lista[32], "JMP");
+    strcpy(lista[33], "JE");
+    strcpy(lista[34], "JG");
+    strcpy(lista[35], "JL");
+    strcpy(lista[36], "JZ");
+    strcpy(lista[37], "JP");
+    strcpy(lista[38], "JN");
+    strcpy(lista[39], "JNZ");
+    strcpy(lista[40], "JNP");
+    strcpy(lista[41], "JNN");
+    strcpy(lista[129], "SYS");
+    strcpy(lista[143], "STOP");
 }
 
 void buscaRotulos(FILE* arch, listaRotulos *rotulos, int mostrar){
@@ -41,7 +88,6 @@ void buscaRotulos(FILE* arch, listaRotulos *rotulos, int mostrar){
     while(!feof(arch)){
         fgets(nextLinea, sizeof(nextLinea), arch);
         sscanf(nextLinea, "%s", next);
-
         if(esRotulo(next)){
             rotulo *nextRotulo = (rotulo*)malloc(sizeof(rotulo));
             nextRotulo->sig = NULL;
@@ -96,4 +142,12 @@ int contieneArg(int argc, char* args[], char* busca){
     return 0;
 }
 
+int esMnemonico(char listaMnemonicos[][5], char mnemonico[]){
+    for(int i=0; i<144; i++){
+        if(strcmp(listaMnemonicos[i], mnemonico))
+    }
+}
 
+int esValido(char *linea){
+    return *linea!='\n' && !(*linea=='/' && *(++linea)=='/');
+}
