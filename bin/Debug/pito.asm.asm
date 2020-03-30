3a0c7d319a09350a253519817cc7f52e41a5956e
luciano izurieta <luchooizurieta@gmail.com>
2020-03-29 21:36:22 -0300

    git ignore

---
+++ bin/Debug/pito.asm.asm
@@ -0,0 +1,25 @@
+XOR FX,FX //inicializo en 0 contador
+MOV [2],0  //inicializo en 0 acumulador
+OTRO: MOV AX,%0001  //leer decimal
+
+MOV DX,1
+JE 0, OTRO
+JE 1, CACA
+JMP CACA
+JMP OTRO
+MOV CX,1  //uso una sola celda para leer
+SYS 1
+CMP [1],0  //se fija si es negativo
+JNP SIGUE  //si es negativo salta
+ADD [2],[1]  //acumulador
+ADD FX,1  //contador
+JMP OTRO
+TPS
+
+SIGUE: CMP 0,FX  //me fijo si el contador esta en 0
+
+JZ fin  //si es 0 salta a fin
+DIV [2],FX
+FIN: MOV DX,2
+SYS 2
+STOP
