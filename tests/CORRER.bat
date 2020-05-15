896878e9b9cd982670710152a7a8c8d6581fb2f1
PeronKun <crgespartano@gmail.com>
2020-05-15 20:19:36 -0300

    set y get memoria y algo mas

--- tests/CORRER.bat
+++ tests/CORRER.bat
@@ -3,14 +3,16 @@ echo TRADUCCION
 echo ==========
 ::mvc.exe 1-aritmetica.asm aritmetica.img
 mvc.exe assembler1.asm codigo1.img
-::mvc.exe assembler2.asm codigo2.img
+echo.
+mvc.exe assembler2.asm codigo2.img
+echo.
+mvc.exe assembler3.asm codigo3.img
 echo.
 echo ====================
 echo TRADUCCION TERMINADA
 echo EJECUCION
 echo ====================
 ::mvx.exe aritmetica.img -d
-mvx.exe codigo1.img -d
-::codigo2.img -d
+mvx.exe codigo1.img codigo2.img codigo3.img
 echo.
 pause
